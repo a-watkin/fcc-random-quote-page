3f9da327a4fa130afbb3ae737282ca422060884d
adam <atomicpenguines@gmail.com>
2018-11-01 02:01:26 +0000

    formatted code and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Random quote page for FCC project
+# Random quote page
+
+A random quote page build for the free code famp front end challanges.
+
+Fetches a random quote from an API and disaplys it on the page along with the source.
+
+Also also for tweeting the current quote.
+
+Uses:
+
+1. jQuery
+2. Bootstrap
+3. Font awesome
+4. restfulAPI from kong
+5. Twitter - retweet code
--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-$( document ).ready(function() {
-    var currentQuote;
-    var currentAuthor;
+$(document).ready(function() {
+  var currentQuote;
+  var currentAuthor;
 
-    function openURL(url){
-        window.open(url, 
-            'Share', 
-            'width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0');
-    }
+  function openURL(url) {
+    window.open(
+      url,
+      "Share",
+      "width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0"
+    );
+  }
 
+  getQuote();
+
+  $("#get-another-quote-button").on("click", function() {
+    // console.log("hello");
     getQuote();
+  });
 
-    $("#get-another-quote-button").on("click", function(){
-        console.log('hello');
-        getQuote();
+  function getQuote() {
+    $.ajax({
+      headers: {
+        "X-Mashape-Key": "mtyC8PM1PzmshPEq4FVntooQ9PsXp1IHWyIjsnPSncF7Si9EAn",
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      url: "https://quotes.p.mashape.com/",
+      success: function(r) {
+        if (typeof r === "string") {
+          r = JSON.parse(r);
+        }
+
+        $("#quote-text").html(r.quote);
+        $("#quote-author").html(r.author + " said that:");
+        $("#quote-source").html("Quote category: " + r.category);
+        // console.log(r + " " + r.quote);
+        // console.log(r.author);
+        // console.log(r.category);
+
+        currentQuote = r.quote;
+        currentAuthor = r.author;
+      }
     });
+  }
 
-    function getQuote() {
-        $.ajax({
-            headers: {
-                "X-Mashape-Key": "mtyC8PM1PzmshPEq4FVntooQ9PsXp1IHWyIjsnPSncF7Si9EAn",
-                Accept: "application/json",
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            url: "https://quotes.p.mashape.com/",
-            success: function(r) {
-            if (typeof r === 'string') {
-                r = JSON.parse(r); 
-            }
-
-            $("#quote-text").html(r.quote);
-            $("#quote-author").html(r.author + " said that:");
-            $("#quote-source").html("Quote category: " + r.category);
-            console.log(r + ' ' + r.quote);
-            console.log(r.author);
-            console.log(r.category);
-
-            currentQuote = r.quote;
-            currentAuthor = r.author;
-            }
-        })
-    }
-
-  $('#tweet-quote').on('click', function() {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
-        + encodeURIComponent('"' 
-        + currentQuote + '" ' 
-        + currentAuthor));
+  $("#tweet-quote").on("click", function() {
+    openURL(
+      "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+        encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+    );
   });
 });
