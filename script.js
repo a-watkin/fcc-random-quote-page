150bb0aa03a4e4c27c6b609ea9840b4af54c7739
a-watkin <atomicpenguines@gmail.com>
2019-02-09 20:24:48 +0000

    test

--- script.js
+++ script.js
@@ -6,43 +6,24 @@ $(document).ready(function() {
     window.open(
       url,
       "Share",
-      "width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0"
+      "width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0"
     );
   }
 
-  getQuote();
-
-  $("#get-another-quote-button").on("click", function() {
-    // console.log("hello");
-    getQuote();
-  });
-
   function getQuote() {
-    console.log("clicked on get new quote");
-    $.ajax({
-      //   headers: {
-      //     "X-Mashape-Key": "Kx8KP5yqLNmshWDGIZHPAvFilBYgp1XSW6bjsnWCbOyKCn62xu",
-      //     Accept: "application/json",
-      //     "Content-Type": "application/x-www-form-urlencoded"
-      //   },
-      url: "https://talaikis.com/api/quotes/random/",
-      success: function(r) {
-        if (typeof r === "string") {
-          r = JSON.parse(r);
-        }
-
-        $("#quote-text").html(r.quote);
-        $("#quote-author").html(r.author + " said that:");
-        // $("#quote-source").html("Quote category: " + r.category);
-        console.log(r + " " + r.quote);
-        console.log(r.author);
-        console.log(r.category);
-
-        currentQuote = r.quote;
-        currentAuthor = r.author;
+    fetch(`https://api.github.com/zen`, {
+      headers: {
+        "Content-Type": "application/json",
+        username: "389fcce7c8a1352976f33821bd4223aad7f50df8"
       }
+    }).then(function(res) {
+      console.log(res, res.json());
     });
   }
+
+  $("#get-another-quote-button").on("click", function() {
+    getQuote();
+  });
 
   $("#tweet-quote").on("click", function() {
     openURL(
